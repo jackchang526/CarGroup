b7a28a9ff8bd74d74e31ef075dacc20065361a1c
贺培文 <hepw@yiche.com>
2017-12-13 17:11:40 +0800

    车系销量排行榜页添加页面输出缓存

--- MWeb/Controllers/SalesRankingController.cs
+++ MWeb/Controllers/SalesRankingController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace MWeb.Controllers
 {
     public class SalesRankingController : Controller
     {
         //
-        // GET: /SalesRanking/
-
+        // GET: /SalesRanking/ 
+        [OutputCache(Duration = 600, Location = OutputCacheLocation.Downstream)]
         public ActionResult Index()
         {
             return View();

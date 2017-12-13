8efe98a2768d79a70d160c0aa1a2e05c4efda53f
贺培文 <hepw@yiche.com>
2017-12-13 16:40:59 +0800

    移动站车系销量排行接口

--- MWeb/Controllers/SalesRankingController.cs
+++ MWeb/Controllers/SalesRankingController.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.UI;
 
 namespace MWeb.Controllers
 {
-    public class RecognitionController : Controller
+    public class SalesRankingController : Controller
     {
         //
-        // GET: /Recognition/
+        // GET: /SalesRanking/
 
-        [OutputCache(Duration = 600, Location = OutputCacheLocation.Downstream)]
         public ActionResult Index()
         {
             return View();

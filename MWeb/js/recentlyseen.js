0e6ad8fb4dd8686eceffa16ff7c4e4c8c6681a9a
贺培文 <hepw@yiche.com>
2017-12-15 14:53:26 +0800

    车系首页猜你喜欢

--- MWeb/js/recentlyseen.js
+++ MWeb/js/recentlyseen.js
@@ -6,14 +6,16 @@
 //
 //Bitauto.Login.init()
 //
-
-
+ 
 //读取浏览过的车
 Bitauto.Login.onComplatedHandlers.push(function (loginRes) { 
     //拿到6个浏览的车cs id 数组
     Bitauto.UserCars.getViewedCars(6, function (cars) {
-        console.log(cars);
-
+        cars = Bitauto.UserCars.viewedcar.arrviewedcar;
+        if (cars == undefined || cars == null) {
+            $(".browse-car").hide();
+            return;
+        }
         var serialInfoUrl = "http://api.car.bitauto.com/carinfo/getserialinfobycsids.ashx?csids=" + cars.join(",");
         $.ajax({
             type: "get",
@@ -31,6 +33,7 @@ Bitauto.Login.onComplatedHandlers.push(function (loginRes) {
                         html += ('<dd><a href="http://car.m.yiche.com/' + data[cars[i]].allSpell + '/">' + data[cars[i]].showName+'</a></dd>'); 
                     } 
                     $(".browse-car dl").html(html);
+                    $(".browse-car").show();
                 }
             },
             error: function (msg) { 
